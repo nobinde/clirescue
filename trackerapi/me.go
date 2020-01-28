82a40a5b121ec3ca5bca70dbac2e747be97bf4d2
nobinde <nobinde@pm.me>
2020-01-28 14:53:58 -0600

    post fork code change

--- trackerapi/me.go
+++ trackerapi/me.go
@@ -8,8 +8,8 @@ import (
 	"os"
 	u "os/user"
 
-	"github.com/GoBootcamp/clirescue/cmdutil"
-	"github.com/GoBootcamp/clirescue/user"
+	"github.com/clirescue/cmdutil"
+	"github.com/clirescue/user"
 )
 
 var (
